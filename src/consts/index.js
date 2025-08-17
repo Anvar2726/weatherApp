a4585655c46ea25258dbb1529eec61ef0b7870e8
Anvar2726 <xidiraliyevanvar@gmail.com>
2025-08-17 15:26:18 +0500

    endpoint changed

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,4 +1,4 @@
-const ENDPOINT = "http://api.weatherapi.com/v1/";
+const ENDPOINT = "https://api.weatherapi.com/v1/";
 const API_KEY = "1732b4e739bc490c8ce93548251108";
 
 export { ENDPOINT, API_KEY };
